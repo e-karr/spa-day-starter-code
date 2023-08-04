7e0388067f3a90af9e1d9d185155502f54dea9ea
Elizabeth Karr <e.karr93@icloud.com>
2023-08-04 10:35:04 -0500

    add validation to form submission

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,7 +3,10 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -16,8 +19,13 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
         model.addAttribute("user", user);
+
+        if (errors.hasErrors()) {
+            return "user/add";
+        }
+
         if (user.getPassword().equals(user.getVerify())) {
            return "user/index";
         }
